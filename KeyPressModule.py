bd8485f481f8e85dc455c3a0de07b529607a8293
being0606 <onuhjin@gmail.com>
2023-11-04 09:03:32 +0900

    initial commit

--- KeyPressModule.py
+++ KeyPressModule.py
@@ -11,7 +11,7 @@ def getKey(keyName):
     for eve in pygame.event.get(): pass
     keyInput = pygame.key.get_pressed()
     myKey = getattr(pygame, 'K_{}'.format(keyName))
-    print('K_{}'.format(keyName))
+    # print('K_{}'.format(keyName)) # key log
 
     if keyInput[myKey]:
         ans = True
